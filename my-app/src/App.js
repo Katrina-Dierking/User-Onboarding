c253f26c247ba616b6a00c4b300785f0c60070cc
Katrina_Dierking <kalynndier@gmail.com>
2019-10-10 21:54:16 -0400

    form complete. Now to submit data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,13 @@
-import React, {useState} from 'react';
-// import {withFormik} from 'formik';
-import UserForm from './components/UserForm';
-import UserData from './components/UserData';
-
+import React from "react";
+import "./App.css";
+import UserForm from "./components/UserForm";
 
 function App() {
-  const [users, setUsers] = useState(UserData);
-
-  const addNewUser = user => {
-    setUsers([...users, user])
-  }
-
   return (
-
-    <div>
-      <div className ="UserSection">
-      <h1>Katrina's Team Members</h1>
-      <UserData userData ={users} />
-      </div>
-
-      <div className ="formSection">
-      <UserForm addNewUser={addNewUser} />
-      </div>
+    <div className="App">
+      <UserForm />
     </div>
   );
 }
-
-
-  
 
 export default App;
--- my-app/src/components/UserForm.js
+++ my-app/src/components/UserForm.js
@@ -1,45 +1,62 @@
 import React from'react';
 import {withFormik, Form, Field} from 'formik';
+import * as yup from 'yup';
 
-const UserForm = (props) => {
-console.log(props)
+
+const UserForm = ({errors, touched}) => {
+  
+console.log(errors)
     return (
 
         
         <Form>
+            <h1>Add a new user below: </h1><br />
 
-        <h1>Add a new team member below: </h1><br />
-            <Field
+                 {touched.name && errors.name && <p className="error">{errors.name}</p>}
+                  <Field
                     type="text"
                     name="name"
-                    placeholder = "name"
+                    placeholder = "* name"
                 
                     />
 
                 <br /><br />
 
+                {touched.email && errors.email && <p className="error">{errors.email}</p>}
                 <Field 
                     type="text"
                     name="email"
-                    placeholder = "email"
+                    placeholder = "* email"
                     
                     />
 
                 <br /><br />
-
+                {touched.password && errors.password && <p className="error">{errors.password}</p>}
                <Field
                     type="password"
                     name="password"
-                    placeholder = "password"
+                    placeholder = "* password"
                    
                     /> 
                 <br /><br />
 
-                <Field
-                    type = "checkbox"
-                    name = "Terms of Service"
-            
-                    /> 
+            <label>
+            {touched.password && errors.terms && <p className="error">{errors.terms}</p>}
+             <Field type = "checkbox" name = "terms" /> 
+             <span>Terms of Service</span>   
+            </label>
+
+            <br></br>
+            <div className = "serviceBox">
+                <Field component = "textarea" name="notes" placeholder="Terms of Services: 
+                is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book." />       
+                </div>         
+
+                <br></br>
+
+                <button type = "sumbit">Submit</button>
         </Form>
     )
 }
@@ -51,8 +68,17 @@ export default withFormik ({
             name: values.name || '',
             email: values.email || '',
             password: values.password || '', 
+            terms: values.terms || false,
+            notes: values.notes || ''
         }
     }, 
+
+    validationSchema: yup.object().shape ({
+        name: yup.string().required('Please enter your name'),
+        email: yup.string().required(),
+        password: yup.string().required(),
+        terms: yup.boolean().required()
+    }),
 
     handleSubmit: (values) => {
         console.log(values)
